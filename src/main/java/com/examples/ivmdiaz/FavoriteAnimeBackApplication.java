30eb6c76db9fdb94b8b1d1bc5f1b36b20d31b15f
Ivan Mauricio Diaz <idiaz@conexia.com>
2021-11-24 20:23:07 -0500

    Initializr

---
+++ src/main/java/com/examples/ivmdiaz/FavoriteAnimeBackApplication.java
@@ -0,0 +1,13 @@
+package com.examples.ivmdiaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteAnimeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteAnimeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examples/ivmdiaz/FavoriteAnimeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.ivmdiaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteAnimeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
